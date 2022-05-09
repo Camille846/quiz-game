a1d7ed1c99f7aaeb43546ee69c58da5da11171fa
Camille <83260908+Camille846@users.noreply.github.com>
2022-05-09 12:43:16 -0300

    Create end.js

---
+++ js/end.js
@@ -0,0 +1,35 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("final-score");
+// const MAX_HIGH_SCORES = 5;
+
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
+
+// const highscores = JSON.parse(localStorage.getItem("highScores"));
+
+// console.log(JSON.parse(localstorage.getItem("highScores")));
+
+username.addEventListener("keyup", () =>{
+    saveScoreBtn.disabled = !username.value;
+})
+
+saveHighScore = (e) =>{
+    e.preventDefault();
+    console.log("clicked")
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value,
+    };  
+    
+    // highscores.push(score);
+
+    // highscores.sort((a,b) => b.score - a.score);
+    // highscores.splice(5);
+
+    // // console.log(highscores);
+    // localStorage.setItem("highscores", JSON.stringify(highscores));
+
+
+}
